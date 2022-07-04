da0b2e171480b365ceec04c23ac8a8f2c993519c
juri.castellani <juri.castellani@accenture.com>
2022-07-04 23:17:25 +0200

    [STREAMS] A Flux example has been added.

--- src/test/java/com/jc/training/java/streams/ExerciseOnGenericsTest.java
+++ src/test/java/com/jc/training/java/streams/ExerciseOnGenericsTest.java
@@ -1,4 +1,19 @@
 package com.jc.training.java.streams;
 
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.assertj.core.api.ListAssert;
+
 public class ExerciseOnGenericsTest {
+    @Test
+    public void test() {
+        List<Integer> elementList = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(elementList::add);
+        ListAssert.assertThatList(elementList).containsExactly(1, 2, 3, 4);
+    }
 }
