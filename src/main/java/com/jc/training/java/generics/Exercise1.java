96ffd0ba0905b2a99c814d460c2e01b72861eda4
juri.castellani <juri.castellani@accenture.com>
2022-07-04 12:01:46 +0200

    [STREAMS] Exercise 1 has been done.

---
+++ src/main/java/com/jc/training/java/generics/Exercise1.java
@@ -0,0 +1,47 @@
+package com.jc.training.java.generics;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        int exerciseNumber = 0;
+        // Use StreamSources.intNumbersStream() and StreamSources.userStream()
+
+        // Print all numbers in the intNumbersStream stream
+        System.out.println("#Exercise 1." + (++exerciseNumber));
+        StreamSources.intNumbersStream().forEach(System.out::println);
+
+        // Print numbers from intNumbersStream that are less than 5
+        System.out.println("#Exercise 1." + (++exerciseNumber));
+        StreamSources.intNumbersStream()
+                .filter(intValue -> intValue < 5)
+                .forEach(System.out::println);
+
+        // Print the second and third numbers in intNumbersStream that's greater than 5
+        System.out.println("#Exercise 1." + (++exerciseNumber));
+        StreamSources.intNumbersStream()
+                .filter(intValue -> intValue > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+
+        //  Print the first number in intNumbersStream that's greater than 5.
+        //  If nothing is found, print -1
+        System.out.println("#Exercise 1." + (++exerciseNumber));
+        System.out.println(StreamSources.intNumbersStream()
+                .filter(intValue -> intValue > 5)
+                .findFirst()
+                .orElse(-1));
+
+        // Print first names of all users in userStream
+        System.out.println("#Exercise 1." + (++exerciseNumber));
+        StreamSources.userStream()
+                .map(User::getFirstName)
+                .forEach(System.out::println);
+
+        // Print first names in userStream for users that have IDs from number stream
+        System.out.println("#Exercise 1." + (++exerciseNumber));
+        StreamSources.userStream()
+                .filter(user -> StreamSources.intNumbersStream().anyMatch(intValue -> intValue.equals(user.getId())))
+                .map(User::getFirstName)
+                .forEach(System.out::println);
+    }
+}
